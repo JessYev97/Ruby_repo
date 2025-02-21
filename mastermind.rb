297a56551c29cd57a3ee905f62df09b39f06815d
JessYev97 <jessmyeverino@gmail.com>
2025-02-21 14:57:14 -0600

    add condition for 'red'
    i added some mess to the code, but this commit is for the conditional in validate_guess. It prevents a feedback of 'red' from triggering if, later in the code, the same color will trigger black and there are no other instances of that color in the code.

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,6 @@
 class Mastermind 
   
-  $round_count
+  $round_count = 0 
   $guess = [] 
   $feedback_array = [] 
   $round_nested_array = []
@@ -68,26 +68,45 @@ def random_guess
     selected_color = @colors[random_num]
     $guess << selected_color 
   end
+  puts "you seeing this step? -- random first guess"
+       answer = gets.chomp 
+       puts answer 
 end
 
 
   def game_loop
-    
+    puts "now we're going to check that info agains the win / loss logic. acknowledge this :)"
+    got_it = gets.chomp 
+    puts got_it 
+
     if $guess == @generated_code or $feedback_array == ["black", "black", "black", "black"]
       puts "YOU WIN! your guess of #{$guess} matches the computer code: #{@generated_code}!"
 return(@generated_code) 
     elsif @all_guesses.length < 8 
-      $round_count = 0
+     
       puts 
-      puts "your guesses so far with their feedback are:"
+      puts "your guesses so far with their feedback are:" 
       puts 
       @all_guesses.each do |round| 
-        round_count += 1 
-        puts "Round: #{round_count} --------------------" 
+        $round_count += 1 
+        puts "Round: #{$round_count} --------------------" 
         puts "Your Guess: #{round[0].inspect}"
         puts "Its Feedback: #{round[1].inspect}" 
       end   #  <---- closing the do end block 
       puts 
+      puts "here is where the logic splits for the computer. It prevents game_loop from emptying the arrays. ackn"
+      ackn = gets.chomp 
+      puts ackn 
+
+      if @computer_path == 1
+      
+        if @all_guesses.length == 8 or $round_count > 8 
+          puts "You're out of guesses. The correct code was: #{@generated_code}" 
+        else 
+          play_round
+        end 
+         
+      end
       $guess.clear  
       $feedback_array.clear
       $round_nested_array.clear 
@@ -103,6 +122,13 @@ return(@generated_code)
     
     color_counts = Hash.new(0) 
 
+    if @computer_path == 1 
+      puts "we're about to iterate over the $guess array"
+    puts "validate that you're with me"
+    respond = gets.chomp 
+    puts respond 
+    end
+
     $guess.each_with_index do |color, index| 
 
       index_position = index 
@@ -111,53 +137,93 @@ return(@generated_code)
       
           if @generated_code[index_position] == color
             $feedback_array << "black" 
-          elsif  @generated_code.include?(color) and @generated_code.count(color) >= color_counts[color]
-                  $feedback_array << "red" 
+          elsif  @generated_code.include?(color) and @generated_code.count(color) >= color_counts[color] 
+            color_index = @generated_code.index(color) 
+            guessed_color_index = $guess.index(color) 
+            if @generated_code.count(color) == 1 and @generated_code[color_index] == $guess[guessed_color_index]
+              $feedback_array << "incorrect"
+            else
+                $feedback_array << "red"  
+            end
+                  
           else 
             $feedback_array << "incorrect" 
           end 
          
-        
+        puts "we just populated the $feedback_array: #{$feedback_array}"
+        puts "pause to acknowledge"
+        tell_em = gets.chomp 
+        puts tell_em 
           
       end 
       puts "the feedback to your guess of #{$guess} is #{$feedback_array}" 
     $round_nested_array << $guess.clone
     $round_nested_array << $feedback_array.clone
     @all_guesses << $round_nested_array.clone
+    puts "now the program should have output the info of the round, and pushed info into the arrays"
+    puts "the $round_nested: #{$round_nested_array} the @all_guesses: #{@all_guesses}" 
+    hmm = gets.chomp 
+    puts hmm 
     game_loop
   end
   
   def educated_guess
+
+    puts "next step: educated_guess. This is before it runs. validate and move on to see how it runs"
+    validate = gets.chomp 
+    puts validate 
+
     $feedback_array.each_with_index do |color, index|  
       next_i = index + 1
       previous_i = index - 1
       if $feedback_array[index] == "black" 
-        @generated_code[index] = color and $guess[index] = @generated_code[index] 
+        @generated_code[index] = color 
+         $guess[index] = @generated_code[index] 
       elsif $feedback_array[index] == "red" 
-        if $feedback_array[next_i] != "black"
+        if next_i < $feedback_array.length and $feedback_array[next_i] != "black"
           $guess[next_i] = color 
-        elsif   previous_i != "black"
+        elsif   previous_i >= 0 and $feedback_array[previous_i] != "black"
           $guess[previous_i] = color 
         else #the color needs to go to the only remaining array index. . . 
+          last_available_index = $guess.each_index.select { |index| $feedback_array[index] != "black" and $guess[index].nil? }.last 
+          $guess[last_available_index] = color if last_available_index 
         end
-      elseif $feedback_array[index] == "incorrect" 
+      elsif $feedback_array[index] == "incorrect" 
         @colors.delete(color) 
       end
     end
+    puts "the current feedback_array is #{$feedback_array}, the round_nested_array is #{$round_nested_array}"
+    puts "the guess array is #{$guess} and all but the guess are about to be cleared by the method."
+    puts "you keeping up? "
+    answer_it = gets.chomp 
+    puts answer_it 
+    $feedback_array.clear
+    $round_nested_array.clear
+    puts "now the feedback array and round nested should be cleared: #{$round_nested_array}, and #{$feedback_array}" 
   end
   
 
   def play_round
     if @human_path == 1
+      puts "there is #{@human_path} person playing!"
       #the human guesses in this path 
       guess_colors  
       validate_guess 
     elsif @computer_path == 1 
+      puts "you seeing this step?"
+      verify_round = gets.chomp 
+      puts verify_round
       #the computer guesses in this path 
-     if $round_count < 2
+     if $round_count < 1
+       puts "you seeing this step? -- the round_count is #{$round_count}. the conditional indicates < 1"
+       answer = gets.chomp 
+       puts answer 
       random_guess
       validate_guess 
      else 
+      puts "you seeing this step? -- the round_count is #{$round_count}. the conditional indicates > 1"
+      answer = gets.chomp 
+      puts answer 
       educated_guess
       validate_guess
      end 
@@ -224,6 +290,6 @@ puts "----------"
 puts "Let's crack the code! You have 8 turns!"
     puts "Black peg means correct color, correct position"
     puts "Red peg means correct color, wrong position"
-    puts "No peg means all colors are incorrect" 
+    puts "incorrect means that colors is nowhere in the code"  
 puts "----------"
 game.determine_roles
